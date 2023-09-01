1da12467275c0305050186860e99825c85547bea
Mike Bell <mbell@landolakes.com>
2023-09-01 16:22:10 -0500

    added command line args

--- src/prefix_reverse_dns_lookup/prefix_reverse_dns_lookup.py
+++ src/prefix_reverse_dns_lookup/prefix_reverse_dns_lookup.py
@@ -1,20 +1,28 @@
+import argparse
 import ipaddress
-import socket
+import dns.resolver
 
-def reverse_dns(ip_address):
+def reverse_dns(ip_address, nameserver='1.1.1.1'):
+   resolver = dns.resolver.Resolver(configure=False)
+   if nameserver:
+      resolver.nameservers = [nameserver]
+   
    try:
-      return socket.gethostbyaddr(str(ip_address))[0]
-   except socket.herror:
+      answers = resolver.resolve(dns.reversename.from_address(str(ip_address)), 'PTR')
+      return str(answers[0])
+   except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.exception.Timeout):
       return None
 
-def reverse_dns_for_prefix(prefix, print_all):
+
+def reverse_dns_for_prefix(prefix, print_all, dns_server=None):
    network = ipaddress.ip_network(prefix, strict=False)
    for ip in network:
-      domain_name = reverse_dns(ip)
+      domain_name = reverse_dns(ip, dns_server)
       if domain_name:
          print(f"{str(ip).ljust(15)} -> {domain_name}")
-      if not domain_name and print_all:
+      elif print_all:
          print(f"{str(ip).ljust(15)} -> Not resolved")
+
 
 def validate_prefix(prefix):
    """Validates the IP prefix with CIDR notation."""
@@ -24,19 +32,45 @@ def validate_prefix(prefix):
    except ValueError:
       return False
 
-if __name__ == '__main__':
-   prefix = ""
-    
-   while not validate_prefix(prefix):
-      prefix = input("Enter the prefix (e.g. 192.0.2.0/24): ")
+def parse_args():
+   parser = argparse.ArgumentParser(description="Perform a reverse DNS lookup on an IP prefix.")
+   
+   # IP Prefix
+   parser.add_argument("--prefix", type=str, default=None, help="IP prefix to scan (e.g., '192.168.1.0/24').")
+   
+   # Print all attempts or just successful resolutions
+   parser.add_argument("--print", type=str, choices=['all', 'successful'], default=None, dest="print_choice", help="Choose to print all attempts or just successful resolutions.")
+   
+   # DNS Server
+   parser.add_argument("--dns-server", type=str, default=None, help="Specify the DNS server's IP for resolution.")
 
-   choice = input("Print all attempts or just successful resolutions? (all/successful): ").strip().lower()
-   if choice.lower().startswith('a'):
-      print_all = True
-   elif choice.lower().startswith('s'):
-      print_all = False
+   return parser.parse_args()
+
+def main():
+   args = parse_args()
+
+   if args.prefix is None or args.print_choice is None:
+      prefix = ""
+      while not validate_prefix(prefix):
+         prefix = input("Enter the prefix (e.g. 192.0.2.0/24): ")
+
+      if args.print_choice is None:
+         choice = input("Print all attempts or just successful resolutions? (all/successful): ").strip().lower()
+         if choice.startswith('a'):
+               print_all = True
+         elif choice.startswith('s'):
+               print_all = False
+         else:
+               print("Invalid choice. Defaulting to printing all attempts.")
+               print_all = True
+      else:
+         print_all = True if args.print_choice == "all" else False
    else:
-      print("Invalid choice. Defaulting to printing all attempts.")
-      print_all = True
-    
-   reverse_dns_for_prefix(prefix, print_all)
+      prefix = args.prefix
+      print_all = True if args.print_choice == "all" else False
+   
+   print(prefix, print_all, args.dns_server)
+   reverse_dns_for_prefix(prefix, print_all, args.dns_server)
+
+if __name__ == '__main__':
+   main()
